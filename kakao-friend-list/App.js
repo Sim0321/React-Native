2698bf0668ffc63511eb2d066be81c2c222ee8ed
Sim <0doogadooga@gmail.com>
2024-09-12 14:20:09 +0900

    친구 목록 scrollView에서 FlatList로 리펙토링

--- kakao-friend-list/App.js
+++ kakao-friend-list/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import Header from "./Header";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Profile from "./Profile";
@@ -18,42 +18,74 @@ export default function App() {
     setIsOpened(!isOpened);
   };
 
+  const ItemSeparatorComponent = () => <Margin height={13} />;
+  const renderItem = ({ item }) => (
+    <View>
+      <Profile
+        uri={item.uri}
+        name={item.name}
+        introduction={item.introduction}
+        isMine={false}
+      />
+    </View>
+  );
+
+  const ListHeaderComponent = () => (
+    <View style={{ backgroundColor: "#fff" }}>
+      <Header />
+      <Margin height={10} />
+      <Profile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+        isMine={true}
+      />
+
+      <Margin height={15} />
+
+      <Division />
+
+      <Margin height={12} />
+
+      <FriendSection
+        friendProfileLen={friendProfiles.length}
+        onPress={onPressArrow}
+        isOpened={isOpened}
+      />
+
+      <Margin height={5} />
+    </View>
+  );
+
+  const ListFooterComponent = () => <Margin height={5} />;
+
   return (
     <SafeAreaProvider>
       <SafeAreaView
         style={styles.container}
         // edges={["right", "top", "left"]}
       >
-        <View style={{ flex: 1, paddingHorizontal: 15 }}>
-          <Header />
-
-          <Margin height={10} />
-
-          <Profile
-            uri={myProfile.uri}
-            name={myProfile.name}
-            introduction={myProfile.introduction}
-          />
-
-          <Margin height={15} />
-
-          <Division />
-
-          <Margin height={12} />
-
-          <FriendSection
-            friendProfileLen={friendProfiles.length}
-            onPress={onPressArrow}
-            isOpened={isOpened}
-          />
-
-          <FriendList data={friendProfiles} isOpened={isOpened} />
-        </View>
-
+        <FlatList
+          data={isOpened ? friendProfiles : []}
+          contentContainerStyle={{ paddingHorizontal: 15 }} // 컨텐츠의 style
+          keyExtractor={(_, index) => index} // key 값
+          ItemSeparatorComponent={ItemSeparatorComponent} // item들 사이의 간격
+          renderItem={renderItem} // item들
+          stickyHeaderIndices={[0]} // 고정할 헤더의 index
+          ListHeaderComponent={ListHeaderComponent} // header
+          ListFooterComponent={ListFooterComponent} // footer
+          showsVerticalScrollIndicator={false}
+        />
         <TabBar
           selectedTabIdx={selectedTabIdx}
           setSelectedTabIdx={setSelectedTabIdx}
         />
+
+        {/* <View style={{ flex: 1,  }}>
+          <FriendList data={friendProfiles} isOpened={isOpened} />
+        </View>
+
+         */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
--- kakao-friend-list/Profile.js
+++ kakao-friend-list/Profile.js
@@ -1,19 +1,32 @@
 import { Image, Text, View } from "react-native";
 import Margin from "./Margin";
 
-export default (props) => {
+export default ({ uri, name, introduction, isMine }) => {
+  const size = isMine ? 50 : 40;
   return (
     <View style={{ flexDirection: "row" }}>
       <Image
-        source={{ uri: props.uri }}
-        style={{ width: 50, height: 50, borderRadius: 20 }}
+        source={{ uri }}
+        style={{ width: size, height: size, borderRadius: "50%" }}
       />
       <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{props.name}</Text>
-        <Margin height={6} />
-        <Text style={{ fontSize: 12, color: "grey" }}>
-          {props.introduction}
+        <Text
+          style={{
+            fontWeight: isMine ? "bold" : "",
+            fontSize: isMine ? 16 : 15,
+          }}
+        >
+          {name}
         </Text>
+
+        {introduction && (
+          <View>
+            <Margin height={isMine ? 6 : 2} />
+            <Text style={{ fontSize: isMine ? 12 : 11, color: "grey" }}>
+              {introduction}
+            </Text>
+          </View>
+        )}
       </View>
     </View>
   );
