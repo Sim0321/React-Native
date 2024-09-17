041abc440da8b9a9313c666382bb38c0ae0d1eff
Sim <0doogadooga@gmail.com>
2024-09-18 01:35:18 +0900

    dayjs를 이용해 달력 데이터 가져오는 util 함수 작성

---
+++ todo-calendar/App.js
@@ -0,0 +1,26 @@
+import dayjs from "dayjs";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { getCalendarColumns } from "./util";
+
+export default function App() {
+  const now = dayjs();
+  console.log(now);
+  const columns = getCalendarColumns(now);
+  console.log(columns);
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
