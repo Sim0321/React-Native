939b436a7f98bb19e613c6163ca6ac8fa1a8464f
Sim <0doogadooga@gmail.com>
2024-09-20 14:19:55 +0900

    date picker 및 전달, 다음달 이동 추가

--- todo-calendar/App.js
+++ todo-calendar/App.js
@@ -10,6 +10,7 @@ import {
 } from "react-native";
 import { getCalendarColumns, getDayColor, getDayText } from "./util";
 import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { useEffect, useState } from "react";
 
 const columnSize = 35;
@@ -19,6 +20,31 @@ export default function App() {
   const [selectedDate, setSelectedDate] = useState(now);
 
   const columns = getCalendarColumns(selectedDate);
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    setSelectedDate(dayjs(date));
+    hideDatePicker();
+  };
+
+  const onPressLeftArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).subtract(1, "month");
+    setSelectedDate(newSelectedDate);
+  };
+  const onPressRightArrow = () => {
+    const newSelectedDate = dayjs(selectedDate).add(1, "month");
+    setSelectedDate(newSelectedDate);
+  };
 
   const Column = ({ text, color, opacity, disabled, onPress, isSelected }) => {
     return (
@@ -62,13 +88,15 @@ export default function App() {
             alignItems: "center",
           }}
         >
-          <ArrowButton name="arrow-left" onPress={() => {}} />
+          <ArrowButton name="arrow-left" onPress={onPressLeftArrow} />
 
-          <Text style={{ fontSize: 20, color: "#404040" }}>
-            {currentDateText}
-          </Text>
+          <TouchableOpacity onPress={showDatePicker}>
+            <Text style={{ fontSize: 20, color: "#404040" }}>
+              {currentDateText}
+            </Text>
+          </TouchableOpacity>
 
-          <ArrowButton name="arrow-right" onPress={() => {}} />
+          <ArrowButton name="arrow-right" onPress={onPressRightArrow} />
         </View>
 
         <View style={{ flexDirection: "row" }}>
@@ -128,6 +156,12 @@ export default function App() {
         renderItem={renderItem}
         numColumns={7}
         ListHeaderComponent={ListHeaderComponent}
+      />
+      <DateTimePickerModal
+        isVisible={isDatePickerVisible}
+        mode="date"
+        onConfirm={handleConfirm}
+        onCancel={hideDatePicker}
       />
     </SafeAreaView>
   );
