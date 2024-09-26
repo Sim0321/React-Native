a2b3954567e0553567093aaccb8f2053264d8ab8
Sim <0doogadooga@gmail.com>
2024-09-26 18:28:46 +0900

    i18n-js 사법

---
+++ translation/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { getLocales } from "expo-localization";
+import { I18n } from "i18n-js";
+
+const i18n = new I18n({
+  en: { welcome: "Hello" },
+  ja: { welcome: "いらっしゃいませ" },
+});
+
+i18n.locale = getLocales()[0].languageCode;
+console.log(i18n.t("welcome"));
+
+export default function App() {
+  const deviceLanguage = getLocales()[0].languageCode;
+  console.log(deviceLanguage);
+  return (
+    <View style={styles.container}>
+      <Text>{deviceLanguage}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
