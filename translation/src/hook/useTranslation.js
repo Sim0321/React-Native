8609015f75ab1d67e2f8e1bf9e2cbe92e2fd16a8
Sim <0doogadooga@gmail.com>
2024-09-27 13:23:50 +0900

    locale async-storage를 이용한 저

--- translation/src/hook/useTranslation.js
+++ translation/src/hook/useTranslation.js
@@ -1,11 +1,14 @@
 import { getLocales } from "expo-localization";
 import { I18n } from "i18n-js";
 import { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const ko = require("../lang/lang.ko.json");
 const en = require("../lang/lang.en.json");
 const ja = require("../lang/lang.ja.json");
 const zh = require("../lang/lang.zh.json");
+
+const LOCALE_KEY = "locale";
 
 const i18n = new I18n({
   ko,
@@ -19,10 +22,26 @@ const i18n = new I18n({
 const deviceLanguage = getLocales()[0].languageCode;
 
 export const useTranslation = () => {
-  const [locale, setLocale] = useState(null);
+  const [locale, _setLocale] = useState(null);
+
+  const setLocale = (v) => {
+    _setLocale(v);
+    AsyncStorage.setItem(LOCALE_KEY, v);
+  };
+
+  const init = async () => {
+    // fromStorage
+    const fs = await AsyncStorage.getItem(LOCALE_KEY);
+    if (fs !== null) {
+      _setLocale(fs);
+    } else {
+      _setLocale(deviceLanguage);
+    }
+    // console.log("fs ::", fs);
+  };
 
   useEffect(() => {
-    setLocale(deviceLanguage);
+    init();
     // setLocale("ja");
   }, []);
 
