aff18d472b2c79a02826a51d9693cd57fcfbca20
Sim <0doogadooga@gmail.com>
2024-09-27 14:42:56 +0900

    스타일 및 배경 Lottie 추가

--- translation/App.js
+++ translation/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { useTranslation } from "./src/hook/useTranslation";
 import Button from "./src/components/Button";
 import { useCookie } from "./src/hook/useCookie";
@@ -6,15 +6,22 @@ import { useCookie } from "./src/hook/useCookie";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect, useState } from "react";
 import LoadingView from "./src/components/LoadingView";
+import LottieView from "lottie-react-native";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const { locale, setLocale, t } = useTranslation();
+  const { locale, setLocale, t, format } = useTranslation();
   const { cookieKey } = useCookie();
   // console.log(t);
 
   const [isLoaded, setIsLoaded] = useState(false);
+
+  const y = new Date().getFullYear();
+  const m = new Date().getMonth() + 1;
+  const d = new Date().getDate();
+
+  const todayText = format(t("today_is"), y, m, d);
 
   useEffect(() => {
     if (cookieKey !== "") {
@@ -32,30 +39,49 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>{t(cookieKey)}</Text>
+      <LottieView
+        autoPlay
+        source={require("./assets/background.json")}
+        resizeMode="cover"
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          zIndex: -1,
+        }}
+      />
 
-      <View style={styles.buttonsContainer}>
-        <Button
-          onPress={() => setLocale("ko")}
-          isSelected={locale === "ko"}
-          text="KO"
-        />
-        <Button
-          onPress={() => setLocale("en")}
-          isSelected={locale === "en"}
-          text="EN"
-        />
-        <Button
-          onPress={() => setLocale("ja")}
-          isSelected={locale === "ja"}
-          text="JA"
-        />
-        <Button
-          onPress={() => setLocale("zh")}
-          isSelected={locale === "zh"}
-          text="ZH"
-        />
-      </View>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View style={styles.topContainer}>
+          <Text style={styles.todayText}>{todayText}</Text>
+          <Text style={styles.cookieText}>{t(cookieKey)}</Text>
+        </View>
+
+        <View style={styles.bottomContainer}>
+          <View style={styles.buttonsContainer}>
+            <Button
+              onPress={() => setLocale("ko")}
+              isSelected={locale === "ko"}
+              text="KO"
+            />
+            <Button
+              onPress={() => setLocale("en")}
+              isSelected={locale === "en"}
+              text="EN"
+            />
+            <Button
+              onPress={() => setLocale("ja")}
+              isSelected={locale === "ja"}
+              text="JA"
+            />
+            <Button
+              onPress={() => setLocale("zh")}
+              isSelected={locale === "zh"}
+              text="ZH"
+            />
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
@@ -64,11 +90,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: "#fff",
-    backgroundColor: "purple",
+    // backgroundColor: "purple",
     alignItems: "center",
     justifyContent: "center",
   },
+
+  topContainer: {
+    flex: 3,
+    // backgroundColor: "pink",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  todayText: {
+    position: "absolute",
+    top: 70,
+    fontSize: 13,
+    color: "#8b658f",
+  },
+  cookieText: {
+    fontSize: 22,
+    color: "#372538",
+    textAlign: "center",
+    marginHorizontal: 30,
+  },
+  bottomContainer: {
+    flex: 1,
+    // backgroundColor: "skyblue",
+    justifyContent: "flex-end",
+  },
+
   buttonsContainer: {
     flexDirection: "row",
+    alignSelf: "center",
+    marginBottom: 25,
   },
 });
