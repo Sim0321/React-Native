ac0be5655ca4daaf684851b709bc9e4e3add5dec
Sim <0doogadooga@gmail.com>
2024-10-02 16:58:29 +0900

    배경화면 다운로드 및 앨범에 저장

--- phone-wallpaper/src/screen/ImageDetailScreen.js
+++ phone-wallpaper/src/screen/ImageDetailScreen.js
@@ -4,14 +4,52 @@ import { Header } from "../components/Header/Header";
 import { useCallback } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { RemoteImage } from "../components/RemoteImage";
+import { Icon } from "../components/Icons";
+import { Button } from "../components/Button";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
 
 export const ImageDetailScreen = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  console.log(route);
+  // console.log("route는?", route);
 
   const onPressBack = useCallback(() => {
     navigation.goBack();
+  }, []);
+
+  const onPressDownload = useCallback(async () => {
+    const downloadResumable = FileSystem.createDownloadResumable(
+      route.params.url,
+      `${FileSystem.documentDirectory}${new Date().getMilliseconds()}.jpg`
+    );
+
+    try {
+      const { uri } = await downloadResumable.downloadAsync();
+      console.log("다운로드 완료!! ", uri);
+
+      const permissionResult = await MediaLibrary.getPermissionsAsync(true);
+      console.log("permissionResult ::", permissionResult);
+
+      if (permissionResult.status === "denied") {
+        // 권한을 거부
+        return;
+      }
+
+      if (permissionResult.status === "undetermined") {
+        // 아직 권한을 부여하거나 거부하지 않음
+        const requestResult = await MediaLibrary.requestPermissionsAsync();
+        console.log("requestResult ::", requestResult);
+        if (requestResult.status === "dinied") {
+          return;
+        }
+      }
+
+      const asset = await MediaLibrary.createAssetAsync(uri);
+      const album = MediaLibrary.createAlbumAsync("MyFirstAlbum", asset, false);
+
+      console.log(album);
+    } catch (error) {}
   }, []);
 
   const { width } = useWindowDimensions();
@@ -31,6 +69,22 @@ export const ImageDetailScreen = () => {
           height={width * 1.5}
         />
       </View>
+
+      <Button onPress={onPressDownload}>
+        <View style={{ paddingBottom: 24, backgroundColor: "black" }}>
+          <View
+            style={{
+              height: 52,
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Typography color="white">DOWNLOAD</Typography>
+            <Icon name="download" size={24} color="white" />
+          </View>
+        </View>
+      </Button>
     </View>
   );
 };
