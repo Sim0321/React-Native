7d3a53bb3b6b3831c568f86817c8ebc60b69d5fd
Sim <0doogadooga@gmail.com>
2024-10-08 18:46:18 +0900

    metaData loading 추가 및 전체삭제

--- webpage-scraper/src/screens/AddLinkScreen.js
+++ webpage-scraper/src/screens/AddLinkScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { Header } from "../components/Header/Header";
-import { useWindowDimensions, View } from "react-native";
+import { ActivityIndicator, useWindowDimensions, View } from "react-native";
 import { useCallback, useState, useEffect } from "react";
 import { SingleLineInput } from "../components/SingleLineInput";
 import { Button } from "../components/Button";
@@ -12,12 +12,15 @@ import { atomLinkList } from "../atoms/atomLinkList";
 import { getOpenGraphData } from "../utils/OpenGraphTagUtils";
 import { RemoteImage } from "./../components/RemoteImage";
 import { getClipboardString } from "../utils/ClipboardUtils";
+import { Icon } from "../components/Icons";
 
 export const AddLinkScreen = () => {
   const navigation = useNavigation();
   const safeAreaInset = useSafeAreaInsets();
   const updateList = useSetRecoilState(atomLinkList);
   const [url, setUrl] = useState("");
+
+  const [loading, setLoading] = useState(false);
 
   const [metaData, setMetaData] = useState(null);
 
@@ -55,19 +58,18 @@ export const AddLinkScreen = () => {
   }, [url, metaData]);
 
   const onSubmitEditing = useCallback(async () => {
+    setLoading(true);
     const result = await getOpenGraphData(url);
-    // console.log("submit ::", url, result);
     setMetaData(result);
+
+    setLoading(false);
   }, [url]);
 
   const onGetClipboardString = useCallback(async () => {
     const result = await getClipboardString();
-    console.log("clipbord Result ::", result);
     if (result.startsWith("http://") || result.startsWith("https://")) {
-      console.log("여기 들어와져요");
       setUrl(result);
       const ogResult = await getOpenGraphData(result);
-      console.log("ogResult ::", ogResult);
       setMetaData({
         title: ogResult.title,
         image: ogResult.image,
@@ -77,8 +79,6 @@ export const AddLinkScreen = () => {
   }, []);
 
   useEffect(() => {
-    // https://fastcampus.co.kr
-    // console.log("여기 되니?");
     onGetClipboardString();
   }, []);
 
@@ -95,52 +95,99 @@ export const AddLinkScreen = () => {
       <View
         style={{
           flex: 1,
-          alignItems: "center",
+          // alignItems: "center",
           justifyContent: "flex-start",
           paddingHorizontal: 24,
           paddingTop: 32,
         }}
       >
-        <SingleLineInput
-          value={url}
-          onChangeText={setUrl}
-          placeholder="https://example.com"
-          onSubmitEditing={onSubmitEditing}
-        />
+        <View>
+          <SingleLineInput
+            value={url}
+            onChangeText={setUrl}
+            placeholder="https://example.com"
+            onSubmitEditing={onSubmitEditing}
+          />
+          <View
+            style={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              right: 0,
+              justifyContent: "center",
+              borderWidth: 1,
+            }}
+          >
+            <Button
+              onPress={() => {
+                setUrl("");
+                setMetaData(null);
+              }}
+            >
+              <Icon name="close" color="black" size={20} />
+            </Button>
+          </View>
+        </View>
 
-        {metaData !== null && (
+        {loading ? (
           <>
             <Spacer space={20} />
             <View
-              style={{
-                borderWidth: 1,
-                borderRadius: 4,
-                borderColor: "gray",
-              }}
+              style={{ borderWidth: 1, borderRadius: 4, borderColor: "gray" }}
             >
-              <RemoteImage
-                url={metaData.image}
-                width={width - 48}
-                height={(width - 48) / 2}
-              />
+              <Spacer space={(width - 48) * 0.5} />
+              <Spacer space={50} />
+
               <View
                 style={{
-                  paddingHorizontal: 12,
-                  paddingVertical: 8,
+                  position: "absolute",
+                  left: 0,
+                  right: 0,
+                  top: 0,
+                  bottom: 0,
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
               >
-                <Spacer space={10} />
-                <Typography fontSize={20} color="black">
-                  {metaData.title}
-                </Typography>
-
-                <Spacer space={4} />
-                <Typography fontSize={16} color="gray">
-                  {metaData.description}
-                </Typography>
+                <ActivityIndicator />
               </View>
             </View>
           </>
+        ) : (
+          metaData !== null && (
+            <>
+              <Spacer space={20} />
+              <View
+                style={{
+                  borderWidth: 1,
+                  borderRadius: 4,
+                  borderColor: "gray",
+                }}
+              >
+                <RemoteImage
+                  url={metaData.image}
+                  width={width - 48}
+                  height={(width - 48) / 2}
+                />
+                <View
+                  style={{
+                    paddingHorizontal: 12,
+                    paddingVertical: 8,
+                  }}
+                >
+                  <Spacer space={10} />
+                  <Typography fontSize={20} color="black">
+                    {metaData.title}
+                  </Typography>
+
+                  <Spacer space={4} />
+                  <Typography fontSize={16} color="gray">
+                    {metaData.description}
+                  </Typography>
+                </View>
+              </View>
+            </>
+          )
         )}
       </View>
 
